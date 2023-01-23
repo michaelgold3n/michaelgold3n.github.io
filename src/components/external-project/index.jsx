c1e13a56c738fb6bc4f97f87330e4245b028f0fa
Michael Golden <67241067+michaelgold3n@users.noreply.github.com>
2023-01-23 11:20:09 -0800

    Update index.jsx

--- src/components/external-project/index.jsx
+++ src/components/external-project/index.jsx
@@ -142,7 +142,7 @@ const ExternalProject = ({ externalProjects, loading, googleAnalytics }) => {
                         skeleton({ width: 'w-40', height: 'h-8' })
                       ) : (
                         <span className="text-base-content opacity-70">
-                          My Projects
+                          What I'm working on
                         </span>
                       )}
                     </h5>
