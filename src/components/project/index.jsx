3e989f80c6111b1ad53ff9dd9884b15d91ea2c98
Michael Golden <67241067+michaelgold3n@users.noreply.github.com>
2023-01-25 11:20:30 -0800

    Update index.jsx

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -135,7 +135,7 @@ const Project = ({ repo, loading, github, googleAnalytics }) => {
                       skeleton({ width: 'w-40', height: 'h-8' })
                     ) : (
                       <span className="text-base-content opacity-70">
-                        Projects
+                        Recent Projects
                       </span>
                     )}
                   </h5>
